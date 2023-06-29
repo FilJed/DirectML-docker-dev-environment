7a91893c8307f28bde807fd9e09cf28e1bdeff7b
Jedy <jedy@live.ru>
2023-06-29 23:35:57 +0300

    reorganize file structure, small error fixes

---
+++ tf/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start the Docker service
+sudo service docker start
+
+# building
+sudo docker build -t tf .
+
+# Run the Docker container
+sudo docker run -it --rm --device /dev/dxg -v ./src:/root/src --mount type=bind,src=/usr/lib/wsl,dst=/usr/lib/wsl -e LD_LIBRARY_PATH=/usr/lib/wsl/lib tf bash
